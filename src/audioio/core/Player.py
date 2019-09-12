6bb12f313fa5b23033ec52680417d4f96debad98
wiccy46 <eric1986_chn@yahoo.com>
2019-09-12 23:29:42 +0200

    Player method fillers

--- src/audioio/core/Player.py
+++ src/audioio/core/Player.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pyaudio
 from .Core import Aiocore
 
 
@@ -6,4 +7,11 @@ class Player(Aiocore):
     """Player class"""
     def __init__(self, sr=44100, bs=1024, device_indices=(None, None)):
         super().__init__()
+        
+    def play(self):
+        print("play audio")
+    
+    def stop(self):
+        """Stop audio whenever"""
+        pass
         
