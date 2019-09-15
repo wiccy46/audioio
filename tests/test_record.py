bb03e1945c8bacba4e0ed9ac67d567a197870234
wiccy46 <eric1986_chn@yahoo.com>
2019-09-15 11:14:24 +0200

    add assert to test import

--- tests/test_record.py
+++ tests/test_record.py
@@ -16,35 +16,38 @@ class Test_Record(unittest.TestCase):
         pass
     
     def test_import(self):
+        """Make sure importing Recorder from audioio or audioio.core are both ok"""
         from src.audioio.core import Recorder
         self.recorder = Recorder(sr=44100, bs=256)
+        self.assertEqual(self.sr, 44100)
         from src.audioio import Recorder
         self.recorder = Recorder(sr=44100, bs=256)
+        self.assertEqual(self.sr, 44100)
 
     def test_stream_open(self):
-
+        """Make sure stream can be opened"""
         self.recorder.record(gain=[0.5])   # Need a better way to validate
         self.assertTrue(self.recorder.record_stream.is_active(), True)
-        # check gain update
         self.assertEqual(self.recorder.input_gains, [0.5])
 
-    # def test_block_mode(self):
-    #     # Block mode recording.:
-    #     self.recorder.record(dur=1., block=True)
-    #     output = np.array(self.recorder.record_buffer).flatten()
-    #     # recording result should be np.float32
-    #     self.assertEqual(output.dtype, 'float32')
-    #     # record length should be correct
-    #     self.assertAlmostEqual(
-    #         output.shape[0] / self.recorder.input_channels 
-    #         / self.recorder.sr, 1., places=2)
+    def test_block_mode(self):
+        """Block mode recording."""
+        self.recorder.record(dur=1., block=True)
+        output = np.array(self.recorder.record_buffer).flatten()
+        # recording result should be np.float32
+        self.assertEqual(output.dtype, 'float32')
+        # record length should be correct
+        self.assertAlmostEqual(
+            output.shape[0] / self.recorder.input_channels 
+            / self.recorder.sr, 1., places=2)
 
-    # def test_nonblock_mode(self):
-    #     self.recorder.record(monitor=True)
-    #     time.sleep(2)
-    #     self.assertTrue(
-    #         self.recorder.record_stream.is_active(), True)
-    #     self.recorder.stop()
-    #     with self.assertRaises(OSError):
-    #         # Stream is not open, should throw OSError
-    #         test = self.recorder.record_stream.is_active()
+    def test_nonblock_mode(self):
+        """Non blocking Mode recording"""
+        self.recorder.record(monitor=True)
+        time.sleep(2)
+        self.assertTrue(
+            self.recorder.record_stream.is_active(), True)
+        self.recorder.stop()
+        with self.assertRaises(OSError):
+            # Stream is not open, should throw OSError
+            test = self.recorder.record_stream.is_active()
